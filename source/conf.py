06ba8c6a1004ad397de6800980f75b099bd2a1f9
hustzxd <2227820226@qq.com>
2020-01-06 21:17:32 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -117,7 +117,7 @@ htmlhelp_basename = 'QuanOviewdoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
+# latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
@@ -133,15 +133,32 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-}
+# }
+latex_elements = {# The paper size ('letterpaper' or 'a4paper').
+'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+'inputenc':'',#必須
+'utf8extra':'',#必須
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-# latex_documents = [
-#     (master_doc, 'QuanOview.tex', 'QuanOview Documentation',
-#      'Xiaodong Shen', 'manual'),
-# ]
+latex_documents = [
+    (master_doc, 'QuanOview.tex', 'QuanOview Documentation',
+     'Xiaodong Shen', 'manual'),
+]
 
 
 # -- Options for manual page output ------------------------------------------
