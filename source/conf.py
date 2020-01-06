77243e8cb5c0e26758aa11f2e6fe84642d3c4c32
hustzxd <2227820226@qq.com>
2020-01-06 21:06:27 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -138,10 +138,10 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'QuanOview.tex', 'QuanOview Documentation',
-     'Xiaodong Shen', 'manual'),
-]
+# latex_documents = [
+#     (master_doc, 'QuanOview.tex', 'QuanOview Documentation',
+#      'Xiaodong Shen', 'manual'),
+# ]
 
 
 # -- Options for manual page output ------------------------------------------
